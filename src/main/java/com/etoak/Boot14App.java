01b79edcd252ccb2be7550760b7832a6f4a4903d
xile <431103832@qq.com>
2020-05-04 23:03:55 +0800

    修改Boot14App启动类

--- src/main/java/com/etoak/Boot14App.java
+++ src/main/java/com/etoak/Boot14App.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication // add comment;
-@EnableScheduling // 启动spring task配置 123
+@SpringBootApplication
+@EnableScheduling // 启动spring task配置
 public class Boot14App {
 
     public static void main(String[] args) {

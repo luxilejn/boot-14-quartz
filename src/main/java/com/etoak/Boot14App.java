edaa18c13581b29bf8bff6301ffbe8343cafdab3
et <et@etoak.com>
2020-05-04 23:23:48 +0800

    main vs 456456456

--- src/main/java/com/etoak/Boot14App.java
+++ src/main/java/com/etoak/Boot14App.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // 启动spring task配置 123123123
+@EnableScheduling // 启动spring task配置 123123123   4564645645
 public class Boot14App {
 
+	// main
     public static void main(String[] args) {
         SpringApplication.run(Boot14App.class, args);
     }
